f6bc56610a423e2d5f23cd15790b626531f54710
mel091 <159938702+mel091@users.noreply.github.com>
2024-02-13 10:04:26 -0700

    Add files via upload

---
+++ 130224/src/example.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+public class example 
+{
+
+	public static void main(String[] args) 
+	{
+		JFrame ventana = new JFrame("Example"); //creamos obj con su nombre
+		ventana.setVisible(true);
+		
+		ventana.setSize(200, 200);
+		ventana.setLocation(100, 100);
+		
+		ventana.setLocationRelativeTo(null);
+		
+		ventana.setResizable(false);
+		
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //deja de correr al cerrarla
+		
+		System.out.println("hi");
+
+	}
+
+}
